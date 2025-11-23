49366015ea725050fdf5f4e4414004b069a28ade
HusseinSelim-1977 <husseinsherif2006@gmail.com>
2025-11-23 18:08:17 +0200

    user dto files Done ✅

--- src/User/user/dto/create-user.dto.ts
+++ src/User/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    currency?: string;
+    prof_pic?: string;
+    salary?: number;
+}
